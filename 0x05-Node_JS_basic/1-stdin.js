467df3b94955502348253101e58936d791de5a5a
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-07-23 16:24:19 +0100

    list of lists

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-const process = require('process')
-
 process.stdout.write("Welcome to Holberton School, what is your name?" + '\n')
 
-process.stdin.on('readable' () => {
-   const name = process.stdin.read();
-   if name !== null {
-     process.stdout.write(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+   const buff = process.stdin.read();
+   if (buff !== null) {
+     process.stdout.write(`Your name is: ${buff}`);
    }
 });
 process.stdin.on('end', () => {
