4d765efd7e3aaddd6ca8efb408448d04576eac99
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2024-07-23 15:24:18 +0100

    readable

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,10 +2,12 @@ const process = require('process')
 
 process.stdout.write("Welcome to Holberton School, what is your name?" + '\n')
 
-process.stdin.on('data', (input) => {
-    const name = input.toString().trim();
-    process.stdout.write(`Your name is: ${name}` + '\n');
+process.stdin.on('readable' () => {
+   const name = process.stdin.read();
+   if name !== null {
+     process.stdout.write(`Your name is: ${name}`);
+   }
 });
-process.on('exit', () => {
-    process.stdout.write("This important software is now closing" + '\n')
+process.stdin.on('end', () => {
+  process.stdout.write("This important software is now closing\n")
 });
